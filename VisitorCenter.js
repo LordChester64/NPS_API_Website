ad836b07b2529909b249fb9be0982cbf7b78bf5c
Ryan <r2barbour@gmail.com>
2019-05-28 10:33:34 -0400

    Set up framework for visitor center map

--- VisitorCenter.js
+++ VisitorCenter.js
@@ -33,4 +33,29 @@ function buildVisitorCenterDetails(visitorCenterInfo, visitorCenterID){
     document.getElementById("visitorCenterNameHeading").innerText = `About ${thisVisitorCenter.name}:`;
     document.getElementById("visitorCenterDescription").innerText = `${thisVisitorCenter.description}`;
     document.getElementById("visitorCenterURL").innerHTML = `<a href="${thisVisitorCenter.url}">Learn More Here!</a>`;
+    displayMap(thisVisitorCenter);
+}
+
+function displayMap(thisVisitorCenter) {
+    var point = parseLatLong(thisVisitorCenter.latLong);
+    console.log(point.lat);
+    console.log(point.long);
+    console.log(config.MAP_KEY);
+    mapboxgl.accessToken = `${config.MAP_KEY}`;
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [point.long, point.lat], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+}
+
+function parseLatLong(latLong){
+    var point = {lat: 0,long: 0};
+    if (latLong.length > 0){
+        var coordinates = latLong.split(",");
+        point.lat = parseFloat(coordinates[0].split(":")[1]);
+        point.long = parseFloat(coordinates[1].split(":")[1].replace("}", ""));
+    }
+    return point;
 }
