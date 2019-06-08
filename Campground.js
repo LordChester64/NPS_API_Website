e75480531e574ebcee30473f1c64109f3048cc8a
Ryan <r2barbour@gmail.com>
2019-06-08 11:54:08 -0400

    Added camp icon to campsite map

--- Campground.js
+++ Campground.js
@@ -1,4 +1,4 @@
-function getCampgroundDescription(){
+function getCampgroundDescription() {
     var searchParams = new URLSearchParams(window.location.href.split("?")[1]);
     var parkCode = searchParams.get("parkCode");
     var campgroundID = searchParams.get("campgroundID");
@@ -14,70 +14,97 @@ async function getCampgroundInfo(code) {
     return await responseData.data;
 }
 
-function buildCampgroundDetails(campgroundInfo, campgroundID){
+function buildCampgroundDetails(campgroundInfo, campgroundID) {
     campgroundList = "";
     countOfCampgrounds = campgroundInfo.length;
-    for (i = 0; i < countOfCampgrounds; i++){
-        if (campgroundID === campgroundInfo[i].id){
+    for (i = 0; i < countOfCampgrounds; i++) {
+        if (campgroundID === campgroundInfo[i].id) {
             thisCampground = campgroundInfo[i];
             break;
         }
     }
     document.getElementById("campgroundName").innerText = `${thisCampground.name}`;
-    if (determineExistance(thisCampground.amenities.campstore) === "Yes"){
+    if (determineExistance(thisCampground.amenities.campstore) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/store.svg" height="30px" width="30px" title="Camp Store" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.firewoodforsale) === "Yes"){
+    if (determineExistance(thisCampground.amenities.firewoodforsale) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/campfire.svg" height="30px" width="30px" title="Firewood for Sale" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.foodstoragelockers) === "Yes"){
+    if (determineExistance(thisCampground.amenities.foodstoragelockers) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/food-cache.svg" height="30px" width="30px" title="Food Storage Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.laundry) === "Yes"){
+    if (determineExistance(thisCampground.amenities.laundry) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/laundry.svg" height="30px" width="30px" title="Laundry Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.toilets[0]) === "Flush"){
+    if (determineExistance(thisCampground.amenities.toilets[0]) === "Flush") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/restrooms.svg" height="30px" width="30px" title="Flush Toilets Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.internetconnectivity) === "Yes"){
+    if (determineExistance(thisCampground.amenities.internetconnectivity) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/wi-fi.svg" height="30px" width="30px" title="Internet Connection Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.potablewater[0]) === "Yes"){
+    if (determineExistance(thisCampground.amenities.potablewater[0]) === "Yes") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/wi-fi.svg" height="30px" width="30px" title="Internet Connection Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.showers[0]) === "Hot"){
+    if (determineExistance(thisCampground.amenities.showers[0]) === "Hot") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/showers.svg" height="30px" width="30px" title="Hot Showers Available" class="amenityPics">`;
     }
-    if (determineExistance(thisCampground.amenities.showers[0]) === "Cold"){
+    if (determineExistance(thisCampground.amenities.showers[0]) === "Cold") {
         document.getElementById("amenityIcons").innerHTML += `<img src="./images/showers.svg" height="30px" width="30px" title="Cold Showers Available" class="amenityPics">`;
     }
     document.getElementById("campgroundNameHeading").innerText = `About ${thisCampground.name}:`;
     document.getElementById("campgroundDescription").innerText = `${thisCampground.description}\n`;
     document.getElementById("campgroundDirectionsHeading").innerText = `Directions:\n`;
-    if (thisCampground.directionsoverview){
+    if (thisCampground.directionsoverview) {
         document.getElementById("campgroundDirections").innerText = `${thisCampground.directionsoverview}`;
     }
-    else{
+    else {
         document.getElementById("campgroundDirections").innerText = "No directions found"
     }
     document.getElementById("campgroundReservationLink").innerHTML = `<a href="${thisCampground.regulationsurl}">Regulations for the campsite</a>`;
-    displayMap(thisCampground);    
+    displayMap(thisCampground);
 }
 
 function displayMap(thisCampground) {
     var point = parseLatLong(thisCampground.latLong);
     mapboxgl.accessToken = `${config.MAP_KEY}`;
     var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [point.long, point.lat], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [point.long, point.lat], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
+    map.on('load', function () {
+        map.loadImage('./images/campsite.jpeg', function (error, image) {
+            if (error) throw error;
+            map.addImage('campsite', image);
+            map.addLayer({
+                "id": "points",
+                "type": "symbol",
+                "source": {
+                    "type": "geojson",
+                    "data": {
+                        "type": "FeatureCollection",
+                        "features": [{
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [point.long, point.lat]
+                            }
+                        }]
+                    }
+                },
+                "layout": {
+                    "icon-image": "campsite",
+                    "icon-size": 0.1
+                }
+            });
+        });
+    });
 }
 
-function parseLatLong(latLong){
-    var point = {lat: 0,long: 0};
-    if (latLong.length > 0){
+function parseLatLong(latLong) {
+    var point = { lat: 0, long: 0 };
+    if (latLong.length > 0) {
         var coordinates = latLong.split(",");
         point.lat = parseFloat(coordinates[0].split(":")[1]);
         point.long = parseFloat(coordinates[1].split(":")[1].replace("}", ""));
@@ -86,12 +113,12 @@ function parseLatLong(latLong){
 }
 
 function determineExistance(value) {
-    if (value){
+    if (value) {
         var splitValue = value.split(" ");
         var ans = splitValue[0];
         return ans;
     }
-    else{
+    else {
         return value;
     }
 }
